98ae5995af845fa5385408d55709a66cb22c1024
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2022-01-05 21:23:53 +0200

    Update script with new folder structure

--- sustainability-gis-installations.sh
+++ sustainability-gis-installations.sh
@@ -11,6 +11,10 @@ NOTEBOOKS_REPO='notebooks'
 # (different years/courses can also be on separate branches of the same repo)
 BRANCH='sustainability-gis-2021'
 
+# Folder name that will appear in the CSC instance
+# This is used to avoid conflicts when cloning if a student has participated to many courses using similar CSC environment
+COURSE_FOLDER='sustainability-gis'
+
 cd /home/jovyan/work
 # git reflog requires a name and email if user is not in passwd
 # even if you're only cloning
@@ -21,18 +25,22 @@ export GIT_COMMITTER_EMAIL=anon@localhost
 SOURCE_REPO="https://github.com/$ORGANIZATION/$NOTEBOOKS_REPO.git"
 
 # Grab notebooks repository
-NOTEBOOK_DIR='/home/jovyan/work/notebooks'
+NB_FOLDER_NAME="$COURSE_FOLDER/notebooks"
+NOTEBOOK_DIR="/home/jovyan/work/$NB_FOLDER_NAME"
+
 if [ -d "$NOTEBOOK_DIR" ]; then
     # Change directories and pull
     cd "$NOTEBOOK_DIR"
     git pull origin $BRANCH
 else
     # Clone the notebooks directory
-    git clone --single-branch --branch $BRANCH $SOURCE_REPO
+    git clone --single-branch --branch $BRANCH $SOURCE_REPO $NB_FOLDER_NAME
 fi
 
 # Create data directory if it doesn't exist
-DATA_DIR='/home/jovyan/work/data'
+DATA_FOLDER_NAME="$COURSE_FOLDER/data"
+DATA_DIR="/home/jovyan/work/$DATA_FOLDER_NAME"
+
 if [ ! -d "$DATA_DIR" ]; then
     mkdir $DATA_DIR
 fi
@@ -43,7 +51,9 @@ fi
 # wget $FILESRC
 
 # Create exercises directory if it doesn't exist
-EXERCISE_DIR='/home/jovyan/work/exercises'
+EX_FOLDER_NAME="$COURSE_FOLDER/exercises"
+EXERCISE_DIR="/home/jovyan/work/$EX_FOLDER_NAME"
+
 if [ ! -d "$EXERCISE_DIR" ]; then
     mkdir $EXERCISE_DIR
 fi
